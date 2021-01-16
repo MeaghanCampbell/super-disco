f76e519bb3d3e5667de3d0441221982222b256d0
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-01-15 21:08:42 -0600

    initial styling HTML Bootstrap and textarea

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+// Display Current Date at top
+
+var currentDate = moment().format('dddd, MMMM Do, YYYY')
+$('#currentDay').append( document.createTextNode(currentDate))
+
+// Blocks color coded if they are past present or future
+
+  /* var pastPresentFuture = function () {
+    var time = $(".time").text().trim()
+    console.log(time); 
+
+    var nine = moment('.hour-nine').set("hour", 9);
+    var ten = moment('.hour-ten').set("hour", 10);
+    var eleven = moment('.hour-eleven').set("hour", 11);
+    var twelve = moment('.hour-twelve').set("hour", 12);
+    var thirteen = moment('.hour-thirteen').set("hour", 13);
+    var fourteen = moment('.hour-fourteen').set("hour", 14);
+    var fifteen = moment('.hour-fifteen').set("hour", 15);
+    var sixteen = moment('.hour-sixteen').set("hour", 16);
+    var seventeen = moment('.hour-seventeen').set("hour", 17);
+  } */
+
+// click into a time block, enter an event
+
+$( ".saveBtn" ).click(function() {
+  var text = $(this)
+    .text()
+    .trim()
+
+  var textInput = $(".description").val()
+  console.log(textInput)
+
+  localStorage.setItem("task", textInput)
+});
+
+// get items when page is refreshed
+
+var loadTasks = function() {
+  tasks = localStorage.getItem("tasks");
+  console.log(tasks)
+}
+
+// pastPresentFuture();
