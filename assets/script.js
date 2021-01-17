ff1202bcaffd521022a913dc2d04d1e3b1549fb8
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-01-16 20:38:59 -0600

    finished local storage

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,8 @@ $('#currentDay').append( document.createTextNode(currentDate))
 
 // Blocks color coded if they are past present or future
 
-  /* var pastPresentFuture = function () {
-    var time = $(".time").text().trim()
+  var pastPresentFuture = function () {
+    var time = $(".time")
     console.log(time); 
 
     var nine = moment('.hour-nine').set("hour", 9);
@@ -18,25 +18,37 @@ $('#currentDay').append( document.createTextNode(currentDate))
     var fifteen = moment('.hour-fifteen').set("hour", 15);
     var sixteen = moment('.hour-sixteen').set("hour", 16);
     var seventeen = moment('.hour-seventeen').set("hour", 17);
-  } */
+  }
 
-// click into a time block, enter an event and save
 
-var textInputValues = [];
+var textInputValues = ["","","","","","","","",""];
 
-$(".saveBtn").click(function () {
+if( localStorage.getItem('task')) {
+  textInputValues = JSON.parse( localStorage.getItem('task'));
+}
+
+// loop through textInputValues array to fill correct textareas
+for(var i = 0; i < textInputValues.length; i++){
+  document.getElementById(`${i}`).value = textInputValues[i]
+}
+
+// click save button
+$(".saveBtn").click(function (event) {
+  
+  // grabbing the first value of the event targets ID and turning it into an number
+  let clickedId = parseInt( event.currentTarget.id[0] )
+
+  // grabbing the value of the click targets sibling textarea
   var textInput = $(this).siblings("textarea").val()
-  console.log(textInput)
 
-  textInputValues.push(textInput)
-  console.log(textInputValues)
+  // putting the text input value into the correct spot in the textInuptValues array
+  textInputValues[ clickedId ] = textInput
 
   saveTasks();
 
 });
 
 // save in local storage
-
 var saveTasks = function() {
     localStorage.setItem("task", JSON.stringify(textInputValues));
 
@@ -44,9 +56,9 @@ var saveTasks = function() {
 }
 
 // get from local storage
-
 var loadTasks = function() {
-  tasks = JSON.parse(localStorage.getItem("task"));
+  tasks = JSON.parse(localStorage.getItem('task'));
 }
 
-// pastPresentFuture();
+
+pastPresentFuture();
