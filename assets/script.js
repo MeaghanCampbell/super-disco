8ec320d675203959a1f5215cb26c7333714fc463
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-01-17 10:03:32 -0600

    add colors to show past present future

--- assets/script.js
+++ assets/script.js
@@ -3,26 +3,8 @@
 var currentDate = moment().format('dddd, MMMM Do, YYYY')
 $('#currentDay').append( document.createTextNode(currentDate))
 
-// Blocks color coded if they are past present or future
-
-  var pastPresentFuture = function () {
-    var past = moment().subtract(1, 'hour')
-    var present = moment().hour()
-    var future = moment().add(1, 'hour')
-    console.log(past)
-
-    var nine = moment('.hour-nine').set("hour", 9);
-    var ten = moment('.hour-ten').set("hour", 10);
-    var eleven = moment('.hour-eleven').set("hour", 11);
-    var twelve = moment('.hour-twelve').set("hour", 12);
-    var thirteen = moment('.hour-thirteen').set("hour", 13);
-    var fourteen = moment('.hour-fourteen').set("hour", 14);
-    var fifteen = moment('.hour-fifteen').set("hour", 15);
-    var sixteen = moment('.hour-sixteen').set("hour", 16);
-    var seventeen = moment('.hour-seventeen').set("hour", 17);
-  }
-
-
+// get present hour
+var present = moment().hour()
 
 var textInputValues = ["","","","","","","","",""];
 
@@ -33,6 +15,13 @@ if( localStorage.getItem('task')) {
 // loop through textInputValues array to fill correct textareas
 for(var i = 0; i < textInputValues.length; i++){
   document.getElementById(`${i}`).value = textInputValues[i]
+  if (9 + i < present) {
+    $(`#${i}`).addClass('past');
+  } else if (9 + i > present) {
+    $(`#${i}`).addClass('future');
+  } else if (9 + i === present) {
+    $(`#${i}`).addClass('present');
+  }
 }
 
 // click save button
@@ -64,4 +53,4 @@ var loadTasks = function() {
 }
 
 
-pastPresentFuture();
+//pastPresentFuture();
